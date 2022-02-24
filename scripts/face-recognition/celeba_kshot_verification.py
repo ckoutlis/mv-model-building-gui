13ad013dd50b1de70bd07c2c58cad8c630b19f12
ckoutlis <ckoutlis@iti.gr>
2022-02-24 15:23:00 +0200

    v0.0.0

--- scripts/face-recognition/celeba_kshot_verification.py
+++ scripts/face-recognition/celeba_kshot_verification.py
@@ -10,87 +10,69 @@ imgdir = f'{drive}DataStorage/CelebA/Img/img_align_celeba/'  # images directory
 identities_fp = f'{drive}DataStorage/CelebA/Anno/identity_CelebA.txt'  # identities .txt filepath
 identities_df = pd.read_csv(identities_fp, header=None, sep=' ')  # identities data frame
 n = 1000  # number of episodes for same and for different identities scenarios
-threshold_ed = np.arange(0.0, 2.005, 0.005)
-threshold_cs = np.arange(-1.0, 1.005, 0.005)
+threshold_distance_range = np.arange(0.0, 2.005, 0.005)
+threshold_similarity_range = np.arange(-1.0, 1.005, 0.005)
 
 print(f'Number of episodes n={n}')
 
 model = feature_extractor()  # load VGGFace feature extraction model
 identities = id2fp(identities_df)  # load a dict mapping between identities and image filepaths
 
-# ACC_ED, ACC_CS, THRESH_ED, THRESH_CS = [], [], [], []
 accuracy_distance, accuracy_similarity, threshold_distance, threshold_similarity = [], [], [], []
 for k in range(1, 11):  # k: number of support set samples - k-shot verification
     image_tuples_same_id = generate_random_image_tuples(identities, k, n, same_identity=True)
     image_tuples_diff_id = generate_random_image_tuples(identities, k, n, same_identity=False)
 
-    DS, DD, SS, SD = [], [], [], []
+    distance_same, distance_different, similarity_same, similarity_different = [], [], [], []
     for i in range(n):
         faces = np.concatenate((get_faces(imgdir, image_tuples_same_id[i]),
                                 get_faces(imgdir, image_tuples_diff_id[i])),
                                axis=0)
         support_same, query_same, support_diff, query_diff = get_embeddings(model, faces, k)
 
-        DS.append(euclidean_distance(support_same, query_same))
-        DD.append(euclidean_distance(support_diff, query_diff))
-        SS.append(cosine_similarity(support_same, query_same))
-        SD.append(cosine_similarity(support_diff, query_diff))
+        distance_same.append(euclidean_distance(support_same, query_same))
+        distance_different.append(euclidean_distance(support_diff, query_diff))
+        similarity_same.append(cosine_similarity(support_same, query_same))
+        similarity_different.append(cosine_similarity(support_diff, query_diff))
 
-    accuracy = []
-    for t in threshold_ed:
-        same = (np.array(DS) < t).astype(int)
-        diff = (np.array(DD) < t).astype(int)
-        TP = np.sum(same == 1)
-        TN = np.sum(diff == 0)
-        FP = np.sum(diff == 1)
-        FN = np.sum(same == 0)
-        accuracy.append((TP + TN) / (TP + TN + FP + FN))
-    indx = np.argmax(accuracy)
-    opt_acc_ed = accuracy[indx]
-    opt_thres_ed = threshold_ed[indx]
-    accuracy_distance.append(opt_acc_ed)
-    threshold_distance.append(opt_thres_ed)
-    print(f'Euclidean distance[k={k}]: best accuracy {opt_acc_ed * 100:1.1f}% for threshold {opt_thres_ed:1.3f}')
+    optimal_accuracy_distance, optimal_threshold_distance = evaluation(
+        thresholds=threshold_distance_range,
+        metric_same=distance_same,
+        metric_different=distance_different,
+        mode='distance',
+        k=k,
+        showtxt=True
+    )
+    accuracy_distance.append(optimal_accuracy_distance)
+    threshold_distance.append(optimal_threshold_distance)
 
-    accuracy = []
-    for t in threshold_cs:
-        same = (np.array(SS) > t).astype(int)
-        diff = (np.array(SD) > t).astype(int)
-        TP = np.sum(same == 1)
-        TN = np.sum(diff == 0)
-        FP = np.sum(diff == 1)
-        FN = np.sum(same == 0)
-        accuracy.append((TP + TN) / (TP + TN + FP + FN))
-    indx = np.argmax(accuracy)
-    opt_acc_cs = accuracy[indx]
-    opt_thres_cs = threshold_cs[indx]
-    accuracy_similarity.append(opt_acc_cs)
-    threshold_similarity.append(opt_thres_cs)
-    print(f'Cosine similarity[k={k}]: best accuracy {opt_acc_cs * 100:1.1f}% for threshold {opt_thres_cs:1.3f}\n')
+    optimal_accuracy_similarity, optimal_threshold_similarity = evaluation(
+        thresholds=threshold_similarity_range,
+        metric_same=similarity_same,
+        metric_different=similarity_different,
+        mode='similarity',
+        k=k,
+        showtxt=True
+    )
+    accuracy_similarity.append(optimal_accuracy_similarity)
+    threshold_similarity.append(optimal_threshold_similarity)
 
-    plt.figure(figsize=(11, 5))
+    plot_histograms(
+        optimal_accuracy_distance,
+        optimal_threshold_distance,
+        optimal_accuracy_similarity,
+        optimal_threshold_similarity,
+        distance_same,
+        distance_different,
+        similarity_same,
+        similarity_different,
+        k,
+        n
+    )
 
-    plt.subplot(1, 2, 1)
-    plt.title(f'Euclidean distance - acc={opt_acc_ed * 100:1.1f}%, thresh={opt_thres_ed:1.3f}\n k={k}, n={n}')
-    y1, _, _ = plt.hist(DS, alpha=0.8, label='same identity')
-    y2, _, _ = plt.hist(DD, alpha=0.8, label='diff. identity')
-    plt.plot([opt_thres_ed, opt_thres_ed], [0, np.max(np.concatenate((y1, y2)))], '-r', linewidth=1.5, label='optimal threshold')
-    plt.legend()
-
-    plt.subplot(1, 2, 2)
-    plt.title(f'Cosine similarity - acc={opt_acc_cs * 100:1.1f}%, thresh={opt_thres_cs:1.3f}\n k={k}, n={n}')
-    y1, _, _ = plt.hist(SS, alpha=0.8, label='same identity')
-    y2, _, _ = plt.hist(SD, alpha=0.8, label='diff. identity')
-    plt.plot([opt_thres_cs, opt_thres_cs], [0, np.max(np.concatenate((y1, y2)))], '-r', linewidth=1.5, label='optimal threshold')
-    plt.legend()
-
-plt.figure()
-plt.plot(np.arange(1, 11), accuracy_distance, '.-', label='accuracy (euc.dist.)')
-plt.plot(np.arange(1, 11), accuracy_similarity, '.-', label='accuracy (cos.sim.)')
-plt.plot(np.arange(1, 11), threshold_distance, '.-', label='threshold (euc.dist.)')
-plt.plot(np.arange(1, 11), threshold_similarity, '.-', label='threshold (cos.sim.)')
-plt.xlabel('k (# support samples)')
-plt.grid()
-plt.legend()
-
-plt.show()
+plot_evaluation(
+    accuracy_distance,
+    accuracy_similarity,
+    threshold_distance,
+    threshold_similarity
+)
--- src/face_recognition.py
+++ src/face_recognition.py
@@ -7,6 +7,24 @@ import os
 from keras_vggface.utils import preprocess_input
 
 
+def feature_extractor():
+    model = VGGFace(model='resnet50')
+    model = Model(inputs=model.inputs, outputs=model.layers[-2].output)
+    return model
+
+
+def id2fp(df):
+    identities = {}
+    for index, row in df.iterrows():
+        image_fp = row[0]
+        identity = row[1]
+        if identity in identities:
+            identities[identity].append(image_fp)
+        else:
+            identities[identity] = [image_fp]
+    return identities
+
+
 def generate_random_image_tuples(identities, k, n, same_identity):
     num_random_identities = 1 if same_identity else 2
     threshold = k + 1 if same_identity else k
@@ -36,24 +54,6 @@ def generate_image_tuple(identities, identity, threshold, same_identity):
                                np.random.choice(identities[identity[1]], 1, replace=False)))
 
 
-def feature_extractor():
-    model = VGGFace(model='resnet50')
-    model = Model(inputs=model.inputs, outputs=model.layers[-2].output)
-    return model
-
-
-def id2fp(df):
-    identities = {}
-    for index, row in df.iterrows():
-        image_fp = row[0]
-        identity = row[1]
-        if identity in identities:
-            identities[identity].append(image_fp)
-        else:
-            identities[identity] = [image_fp]
-    return identities
-
-
 def get_faces(directory, tuples):
     support = [os.path.join(directory, x) for x in tuples[:-1]]
     query = os.path.join(directory, tuples[-1])
@@ -81,3 +81,82 @@ def euclidean_distance(x, y):
 
 def cosine_similarity(x, y):
     return np.dot(x, y) / (np.linalg.norm(x) * np.linalg.norm(y))
+
+
+def evaluation(thresholds, metric_same, metric_different, mode, k, showtxt=True):
+    accuracy = []
+    for threshold in thresholds:
+        if mode == 'distance':
+            same = (np.array(metric_same) < threshold).astype(int)
+            diff = (np.array(metric_different) < threshold).astype(int)
+        elif mode == 'similarity':
+            same = (np.array(metric_same) > threshold).astype(int)
+            diff = (np.array(metric_different) > threshold).astype(int)
+        tp = np.sum(same == 1)
+        tn = np.sum(diff == 0)
+        fp = np.sum(diff == 1)
+        fn = np.sum(same == 0)
+        accuracy.append((tp + tn) / (tp + tn + fp + fn))
+    index = np.argmax(accuracy)
+    if showtxt:
+        if mode == 'distance':
+            print(f'Euclidean distance[k={k}]: best accuracy {accuracy[index] * 100:1.1f}% '
+                  f'for threshold {thresholds[index]:1.3f}')
+        elif mode == 'similarity':
+            print(f'Cosine similarity[k={k}]: best accuracy {accuracy[index] * 100:1.1f}% '
+                  f'for threshold {thresholds[index]:1.3f}\n')
+    return accuracy[index], thresholds[index]
+
+
+def plot_histograms(optimal_accuracy_distance,
+                    optimal_threshold_distance,
+                    optimal_accuracy_similarity,
+                    optimal_threshold_similarity,
+                    distance_same,
+                    distance_different,
+                    similarity_same,
+                    similarity_different,
+                    k,
+                    n):
+
+    plt.figure(figsize=(11, 5))
+
+    plt.subplot(1, 2, 1)
+    plt.title(f'Euclidean distance - acc={optimal_accuracy_distance * 100:1.1f}%, '
+              f'thresh={optimal_threshold_distance:1.3f}\n k={k}, n={n}')
+    y1, _, _ = plt.hist(distance_same, alpha=0.8, label='same identity')
+    y2, _, _ = plt.hist(distance_different, alpha=0.8, label='diff. identity')
+    plt.plot([optimal_threshold_distance, optimal_threshold_distance],
+             [0, np.max(np.concatenate((y1, y2)))],
+             '-r',
+             linewidth=1.5,
+             label='optimal threshold')
+    plt.legend()
+
+    plt.subplot(1, 2, 2)
+    plt.title(f'Cosine similarity - acc={optimal_accuracy_similarity * 100:1.1f}%, '
+              f'thresh={optimal_threshold_similarity:1.3f}\n k={k}, n={n}')
+    y1, _, _ = plt.hist(similarity_same, alpha=0.8, label='same identity')
+    y2, _, _ = plt.hist(similarity_different, alpha=0.8, label='diff. identity')
+    plt.plot([optimal_threshold_similarity, optimal_threshold_similarity],
+             [0, np.max(np.concatenate((y1, y2)))],
+             '-r',
+             linewidth=1.5,
+             label='optimal threshold')
+    plt.legend()
+
+    basepath = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+    plt.savefig(os.path.join(basepath, f'results/face-recognition/figs/fig_k{k}.jpg'))
+
+
+def plot_evaluation(accuracy_distance, accuracy_similarity, threshold_distance, threshold_similarity):
+    plt.figure()
+    plt.plot(np.arange(1, 11), accuracy_distance, '.-', label='accuracy (euc.dist.)')
+    plt.plot(np.arange(1, 11), accuracy_similarity, '.-', label='accuracy (cos.sim.)')
+    plt.plot(np.arange(1, 11), threshold_distance, '.-', label='threshold (euc.dist.)')
+    plt.plot(np.arange(1, 11), threshold_similarity, '.-', label='threshold (cos.sim.)')
+    plt.xlabel('k (# support samples)')
+    plt.grid()
+    plt.legend()
+    basepath = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+    plt.savefig(os.path.join(basepath, f'results/face-recognition/figs/fig_evaluation.jpg'))
