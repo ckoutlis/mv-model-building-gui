71f69e625c0fa1d16beb62a238b5b4212a434bf7
ckoutlis <ckoutlis@iti.gr>
2022-03-04 16:17:09 +0200

    Few-shot image classification experiments.

---
+++ scripts/image-classification/experiments/experiments.py
@@ -0,0 +1,61 @@
+from src.image_classification.experiment import *
+
+device = torch.device('cuda:0' if torch.cuda.is_available() else 'cpu')
+
+# Hyper-parameter grid
+models = [
+    'resnet18',
+    # 'vit_small_patch16_224_in21k',
+    # 'resnetv2_50x1_bitm_in21k',
+    # 'resnetv2_101x3_bitm_in21k'
+]
+unfreezed_list = [
+    0.0,
+    # 0.3,
+    # 'all'
+]
+learning_rates = [
+    # 0.001,
+    0.003,
+]
+batch_sizes = [
+    # 8,
+    # 16,
+    # 32,
+    # 64,
+    # 128,
+    # 256,
+    512
+]
+K = [
+    5,
+    # 10,
+    # 50,
+]
+iterations_list = [
+    500,
+    # 1000,
+    # 2000,
+    # 3000
+]
+experiments = 1
+
+# save results path
+datasets = ['cifar10']
+drive = '/home/ckoutlis/disk_2_ubuntu/home/ckoutlis/'
+savdir = f'{drive}PycharmProjects/mv-model-building-gui/results/image-classification/'
+
+for dataset in datasets:
+    savpath = f'{savdir}{dataset}_accuracy.pickle'
+    run(
+        device,
+        dataset,
+        savpath,
+        models,
+        unfreezed_list,
+        learning_rates,
+        batch_sizes,
+        K,
+        iterations_list,
+        experiments
+    )
--- src/image_classification/experiment.py
+++ src/image_classification/experiment.py
@@ -1,68 +1,119 @@
 import os
 import numpy as np
+import pickle
+import time
+import timm
+import torch.optim as optim
 import torch
 import torchvision
 import torchvision.transforms as transforms
 
 
-def load_train_data(model, batch_size, k):
+def load_train_data(dataset, model, batch_size, k):
     # mv-model-building-gui absolute path
     basepath = os.path.dirname(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
-    autoaugment = transforms.AutoAugment()
-    totensor = transforms.ToTensor()
-    resize = transforms.Resize(224)
-    normalize = transforms.Normalize(mean=[0.485, 0.456, 0.406],
-                                     std=[0.229, 0.224, 0.225])
-    if 'vit' in model:
-        transform = transforms.Compose([autoaugment, totensor, resize, normalize])
+
+    precrop = 256 if 'vit' in model else 160
+    crop = 224 if 'vit' in model else 128
+    transform = transforms.Compose(
+        [
+            transforms.Resize(precrop),
+            transforms.RandomCrop(crop),
+            transforms.RandomHorizontalFlip(),
+            transforms.ToTensor(),
+            transforms.Normalize(
+                mean=[0.5, 0.5, 0.5],
+                std=[0.5, 0.5, 0.5]
+            )
+        ]
+    )
+
+    if dataset == 'cifar10':
+        train = torchvision.datasets.CIFAR10(
+            root=f'{basepath}/data/image-classification',
+            train=True,
+            download=False,
+            transform=transform
+        )
     else:
-        transform = transforms.Compose([autoaugment, totensor, normalize])
+        raise Exception(f'{dataset} dataset is not implemented.')
 
-    trainset = torchvision.datasets.CIFAR10(root=f'{basepath}/data/image-classification', train=True, download=False, transform=transform)
-    trainloader = torch.utils.data.DataLoader(trainset, batch_size=1, shuffle=True, num_workers=2)
-
-    traindata = []
+    loader = torch.utils.data.DataLoader(
+        train,
+        batch_size=1,
+        shuffle=True,
+        num_workers=2
+    )
+    train = []
     class_size = [0] * 10
-    for data in trainloader:
+    for data in loader:
         inputs, labels = data
-
         for c in range(10):
             if labels[0] == c and class_size[c] < k:
-                traindata.append([inputs, labels])
+                train.append([inputs.squeeze(), labels.squeeze()])
                 class_size[c] += 1
-
         if all([s == k for s in class_size]):
             break
 
-    batched_train_data = []
-    for i in range(np.ceil(len(traindata) / batch_size).astype(int)):
-        batch_image = []
-        batch_label = []
-        for data in traindata[i * batch_size:(i + 1) * batch_size]:
-            batch_image.append(data[0])
-            batch_label.append(data[1])
-        batched_train_data.append([torch.cat(batch_image, 0), torch.cat(batch_label, 0)])
-    return batched_train_data
+    if batch_size <= len(train):
+        loader = torch.utils.data.DataLoader(
+            train,
+            batch_size=batch_size,
+            shuffle=True,
+            num_workers=2,
+            pin_memory=True,
+            drop_last=False
+        )
+    else:
+        loader = torch.utils.data.DataLoader(
+            train,
+            batch_size=batch_size,
+            num_workers=2,
+            pin_memory=True,
+            sampler=torch.utils.data.RandomSampler(
+                train,
+                replacement=True,
+                num_samples=batch_size
+            )
+        )
+
+    return loader
 
 
-def load_test_data(model, batch_size):
+def load_test_data(dataset, model, batch_size):
     # mv-model-building-gui absolute path
     basepath = os.path.dirname(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
 
-    autoaugment = transforms.AutoAugment()
-    totensor = transforms.ToTensor()
-    resize = transforms.Resize(224)
-    normalize = transforms.Normalize(mean=[0.485, 0.456, 0.406],
-                                     std=[0.229, 0.224, 0.225])
-    if 'vit' in model:
-        transform = transforms.Compose([autoaugment, totensor, resize, normalize])
+    size = 224 if 'vit' in model else 160
+    transform = transforms.Compose(
+        [
+            transforms.Resize(size),
+            transforms.ToTensor(),
+            transforms.Normalize(
+                mean=[0.5, 0.5, 0.5],
+                std=[0.5, 0.5, 0.5]
+            )
+        ]
+    )
+
+    if dataset == 'cifar10':
+        test = torchvision.datasets.CIFAR10(
+            root=f'{basepath}/data/image-classification',
+            train=False,
+            download=False,
+            transform=transform
+        )
     else:
-        transform = transforms.Compose([autoaugment, totensor, normalize])
+        raise Exception(f'{dataset} dataset is not implemented.')
 
-    testset = torchvision.datasets.CIFAR10(root=f'{basepath}/data/image-classification', train=False, download=False, transform=transform)
-    testloader = torch.utils.data.DataLoader(testset, batch_size=batch_size, shuffle=False, num_workers=2)
+    loader = torch.utils.data.DataLoader(
+        test,
+        batch_size=batch_size,
+        shuffle=False,
+        num_workers=2
+    )
 
-    return testloader
+    return loader
 
 
 def freeze_conv(network, unfreezed):
@@ -107,7 +158,7 @@ def freeze_blocks(network, unfreezed):
     return network
 
 
-def train(network, device, loader, iterations, optimizer, criterion, showtxt):
+def train(network, device, loader, iterations, optimizer, learning_rate, criterion, showtxt):
     iteration = 0
     flag = False
     while True:
@@ -125,6 +176,13 @@ def train(network, device, loader, iterations, optimizer, criterion, showtxt):
 
             if showtxt:
                 print(f'[{iteration}] loss: {loss.item():.3f}')
+
+            if iteration in [int(iterations * 0.3),
+                             int(iterations * 0.6),
+                             int(iterations * 0.9)]:
+                learning_rate /= 10
+                for g in optimizer.param_groups:
+                    g['lr'] = learning_rate
 
             if iteration == iterations:
                 flag = True
@@ -148,3 +206,97 @@ def evaluate(network, device, loader):
             correct += (predicted == labels).sum().item()
     accuracy = correct / total
     return accuracy
+
+
+def run(
+        device,
+        dataset,
+        savpath,
+        models,
+        unfreezed_list,
+        learning_rates,
+        batch_sizes,
+        K,
+        iterations_list,
+        experiments):
+
+    start_experiments = time.time()
+
+    # Total number of experiments to be conducted
+    total = len(models) * len(unfreezed_list) * len(learning_rates) * len(batch_sizes) * len(K) * len(iterations_list) * experiments
+    results = []
+    for model in models:
+        test_loader = load_test_data(dataset, model, 128)
+        for unfreezed in unfreezed_list:
+            for learning_rate in learning_rates:
+                for batch_size in batch_sizes:
+                    for k in K:
+                        for iterations in iterations_list:
+                            accuracies = []
+                            times = []
+                            for experiment in range(experiments):
+                                start = time.time()
+
+                                print(f'{experiment + 1}..', end='')
+
+                                train_loader = load_train_data(dataset, model, batch_size, k)
+                                for data in train_loader:
+                                    inputs, labels = data
+
+                                network = timm.create_model(
+                                    model,
+                                    pretrained=True,
+                                    num_classes=10
+                                ).to(device)
+                                if 'vit' in model:
+                                    network = freeze_blocks(network, unfreezed)
+                                else:
+                                    network = freeze_conv(network, unfreezed)
+                                criterion = torch.nn.CrossEntropyLoss()
+                                optimizer = optim.SGD(network.parameters(), lr=learning_rate, momentum=0.9)
+
+                                network = train(
+                                    network=network,
+                                    device=device,
+                                    loader=train_loader,
+                                    iterations=iterations,
+                                    optimizer=optimizer,
+                                    learning_rate=learning_rate,
+                                    criterion=criterion,
+                                    showtxt=False
+                                )
+
+                                accuracy = evaluate(
+                                    network=network,
+                                    device=device,
+                                    loader=test_loader
+                                )
+
+                                accuracies.append(accuracy)
+
+                                end = time.time()
+
+                                times.append(end - start)
+
+                            result = {
+                                'config': {
+                                    'model': model,
+                                    'unfreezed': unfreezed,
+                                    'learning_rate': learning_rate,
+                                    'batch_size': batch_size,
+                                    'k': k,
+                                    'iterations': iterations
+                                },
+                                'accuracy': accuracies
+                            }
+                            results.append(result)
+
+                            with open(savpath, 'wb') as h:
+                                pickle.dump(results, h, protocol=pickle.HIGHEST_PROTOCOL)
+
+                            print(
+                                f'\n[{len(results) * experiments}/{total}] ETA: {np.mean(times) * (total - len(results)) / 3600:1.2f} h')
+                            print(result)
+                            print()
+
+    print(f'Total time: {(time.time() - start_experiments) / 3600:1.2f} h')
